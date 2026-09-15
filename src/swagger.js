1717eaf2bba89ce6491863a4049ca105afce3c5d
vaibhavdhake123 <dhakev1@gmail.com>
2026-09-15 14:02:39 +0530

    swgger fix

--- src/swagger.js
+++ src/swagger.js
@@ -1,24 +1,16 @@
 const path = require("path");
 const swaggerJsdoc = require("swagger-jsdoc");
 
-const port = Number(process.env.PORT) || 5000;
-
-const baseUrl = process.env.API_BASE_URL || `http://localhost:${port}`;
-
 const swaggerSpec = swaggerJsdoc({
     definition: {
         openapi: "3.0.3",
+
         info: {
             title: "Mobile Apps Analytics API",
             version: "1.0.0",
             description: "Analytics API for mobile applications",
         },
-        servers: [
-            {
-                url: baseUrl,
-                description: "API Server",
-            },
-        ],
+
         tags: [
             {
                 name: "Analytics",
@@ -26,6 +18,7 @@ const swaggerSpec = swaggerJsdoc({
             },
         ],
     },
+
     apis: [path.join(__dirname, "routes/*.js")],
 });
 
